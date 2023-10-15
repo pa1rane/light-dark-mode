cf9dc58017340c023270791e6bb4b69d848bd044
Pavan Rane <ranepavanrp@gmail.com>
2023-10-15 14:11:58 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const [darkMode, setDarkMode] = useState(false);
+
+  const containerStyle = {
+      backgroundColor : "lightBlue",
+      padding: "20px",
+      color : " black"
+  }
+  
+  const containerStyleDark = {
+      backgroundColor : "black",
+      padding: "20px",
+      color : "white"
+  }
+
+
+  const toggleStyle = {
+    backgroundColor: "white",
+    color: "black",
+    padding: "5px",
+    borderColor : "black"
+  }
+  const toggleStyleDark = {
+    backgroundColor: "black",
+    color: "white",
+    padding: "5px",
+    borderColor : "white"
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div> 
+    <div className='container' style={darkMode ? containerStyle : containerStyleDark}>
+      <h1 className='heading'>Try to toggle the theme and see the change !!!</h1>
+      <button className='toggle' style={darkMode ? toggleStyle : toggleStyleDark} onClick={() => setDarkMode(!darkMode)}>Toggle Theme</button>
     </div>
-  );
+    </div>
+  )
 }
 
-export default App;
+export default App
+
